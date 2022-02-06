08372b6151853023bfd92e9c5c808e1da46ebc7d
syafnamp <syafnamayshandaap@gmail.com>
2022-02-06 17:12:03 +0700

    membuat tabel database datapasien

---
+++ database/migrations/2022_02_06_095844_create_datapasien_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatapasienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datapasien', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->integer('umur');
+            $table->longText('alamat');
+            $table->string('status');
+            $table->integer('tgllahir');
+            $table->string('jeniskelamin');
+            $table->longText('keluhan');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datapasien');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,3 @@ use App\Http\Controllers\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/login', [AuthController::class, 'login']);
-
-Route::get('/register', [AuthController::class, 'register']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
-    return 'hello world';
-});
-Route::get('/article', [ArticleController::class, 'index']);
-Route::get('/article/list', [ArticleController::class, 'list']);
-Route::get('/article/edit/{slug}', [ArticleController::class, 'edit']);
-Route::get('/article/insert', [ArticleController::class, 'insert']);
-Route::get('/article/store', [ArticleController::class, 'store']);
-Route::get('/article/update', [ArticleController::class, 'update']);
-
-Route::get('/content', [ContentController::class, 'index']);
-Route::get('/content/input', [ContentController::class, 'input']);
