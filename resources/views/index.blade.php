0fec86c80cfcb3ecb335889af1bfb51b6ee7c0f5
syafnamp <syafnamayshandaap@gmail.com>
2022-02-06 18:02:04 +0700

    membuat controllerpasien dan view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+    <section>
+        <div class="container mt-5">
+            <div class="row">
+            <div class="col-lg-8">
+                <h1>Hallo></h1>
+
+                
+            </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PasienController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +13,10 @@ use App\Http\Controllers\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', function () {
+//   return view('welcome');
+// });
+
+Route::get('/'(PasienController::class,'index'));
+
